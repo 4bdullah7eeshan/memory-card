62e4f1e08b637fcfe4f091baec0589e837c230a5
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-21 15:05:20 +0530

    feat(styles): remove github icon and center text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
 function Footer() {
     return (
         <footer>
             <div>
-                <p>Copyright &copy; Abdullah Zeeshan 2024</p>
-            </div>
-            <div>
                 <a href="https://github.com/4bdullah7eeshan" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} />
+                    Copyright &copy; Abdullah Zeeshan 2024
                 </a>
             </div>
         </footer>
