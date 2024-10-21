9c0d61c2ebac419236f2469114df264a0d30e388
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-21 11:39:18 +0530

    feat: create a Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className='logo'>
+            <h1>
+                Memory Card
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
